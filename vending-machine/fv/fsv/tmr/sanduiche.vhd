79895be5988696caae22ce27d08bfc5c79d978de
Alexandre Amory <amamory@gmail.com>
2019-07-25 12:23:06 -0300

    including the tmr version of the vending machine, where all faults are safe

---
+++ vending-machine/fv/fsv/tmr/sanduiche.vhd
@@ -0,0 +1,119 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity sanduiche is
+    port (
+           clock:   in STD_LOGIC;
+           reset:   in STD_LOGIC;
+           M100:    in STD_LOGIC;
+           DEV:     in STD_LOGIC;
+           R_green: in STD_LOGIC;
+           R_atum:  in STD_LOGIC;
+           R_bacon: in STD_LOGIC;
+
+           busy:  out STD_LOGIC;
+           D100:  out STD_LOGIC;
+           GREEN: out STD_LOGIC;
+           ATUM: out STD_LOGIC;
+           BACON: out STD_LOGIC
+       );
+end sanduiche;
+
+architecture sanduiche of sanduiche is
+
+        type STATES is (Action, SOMA, Sb, Sa, Sg, NULO, DEVOL); 
+        signal EA, PE : STATES;   
+    
+        signal erro  :   std_logic;
+        signal grana  :  std_logic_vector(4 downto 0);
+
+begin    
+ 
+    --
+    --  tratamento de erro
+    -- 
+    erro <=  (R_bacon and R_atum) or (R_bacon and R_green) or (R_atum and R_green);
+    --
+    --  máquina de estados
+    -- 
+    process(reset,clock)
+    begin
+        if reset='1' then 
+             EA <= Action;
+        elsif rising_edge(clock) then
+             EA <= PE;
+        end  if;
+     end process;
+
+    process(EA, M100, DEV, R_bacon, R_green, R_atum, erro, grana)
+    begin
+       
+          case EA is
+
+             when Action =>  if DEV='1' or erro='1' then
+                                   PE <= NULO;
+                             elsif  M100='1' and grana < 31 then
+                                  PE <= SOMA;
+                             elsif R_bacon='1' and grana>=4 then
+                                   PE <= Sb; 
+                             elsif R_atum='1'  and grana>=3 then
+                                   PE <= Sa; 
+                             elsif R_green='1' and grana>=2 then
+                                   PE <= Sg;       
+                             elsif R_bacon='1' or R_atum='1' or R_green='1' then
+                                  PE <= NULO;
+                             else
+                                  PE <= Action;
+                             end if;
+
+             when SOMA =>  PE <= Action;  
+                                 
+             when Sb | Sa | Sg  =>  PE<= NULO;
+                                 
+             when NULO =>  if grana=0 then
+                                PE <= Action;  
+                           else 
+                                PE <= DEVOL; 
+                          end if;
+                                  
+             when DEVOL =>  PE <= NULO; 
+          end case;
+
+    end process;
+
+    --
+    --  saídas dp circuito
+    -- 
+    GREEN <= '1'  when  EA=Sg     else '0';  
+    ATUM  <= '1'  when  EA=Sa     else '0';  
+    BACON <= '1'  when  EA=Sb     else '0';  
+    busy  <= '0'  when  EA=Action else '1';  
+    -- does not allow to add more coins than the max of 31
+    D100  <= '1'  when  (EA=NULO and grana>0) or (EA=Action and grana=31 and M100='1' )  else '0';  
+
+    --
+    --  contador do dinheiro acumulado
+    --
+    process (clock, reset)
+      begin
+        if reset ='1' then  
+                 grana <= (others=>'0');
+        elsif rising_edge(clock) then 
+
+               if EA=SOMA then
+                  grana <= grana + 1;
+               elsif EA=Sg then
+                  grana <= grana - 2;
+               elsif EA=Sa then
+                  grana <= grana - 3;
+                elsif EA=Sb then
+                  grana <= grana - 4;
+                elsif EA=DEVOL then
+                  grana <= grana - 1;
+              end if;
+
+        end if;
+    end process;
+              
+end sanduiche;
---
+++ vending-machine/fv/fsv/tmr/tb_ft.vhd
@@ -0,0 +1,86 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb is
+end tb;
+
+architecture tb of tb is
+ 
+  signal clock, reset, M100, DEV, R_green, R_atum, R_bacon: STD_LOGIC := '0' ;
+  signal D100, GREEN, ATUM, BACON, busy: STD_LOGIC;
+
+  type input_data is array (natural range <>) of std_logic_vector(4 downto 0);
+
+  constant program : input_data :=   --   M100  DEV  R_green R_atum R_bacon
+         ( "10000",   -- M100
+           "10000",   -- M100
+           "10000",   -- M100
+           "10000",   -- M100
+           --   "00000" ,   -- nada
+
+           "10000",   -- M100
+           "10000",   -- M100
+           "10000",   -- M100    --   R$ 7 reais
+           "00001",   -- R_bacon 
+           "10000",   -- M100
+           "10000",   -- M100    --  R$ 2 reas
+           "00011",   -- R_atum R_bacon  -- erro
+           "10000",   -- M100
+           "10000",   -- M100    --  R$ 2 reas
+           "01000",   -- devolução
+           "10000",   -- M100
+           "10000",   -- M100
+           "10000",   -- M100    --   R$ 3 reais
+           "00010",   -- R_atum
+           "10000",   -- M100
+           "10000",   -- M100
+           "10000",   -- M100    --   R$ 3 reais
+           "00100",   -- R_green
+           "10000",   -- M100
+           "00001",   -- R_bacon   -- pede bacon sem saldo
+           "00000",    -- nada
+           "00000",    -- nada
+           "00000",    -- nada
+           "11111"    -- nada
+         );  
+begin
+
+   reset <= '1', '0' after 3 ns;         -- reset  
+   clock <= not clock after 5 ns;        -- clock with a period = 10ns
+
+   -- circuit under test
+   sandwich: entity work.sanduiche port map (clock=>clock, reset=>reset,
+              M100=>M100, DEV=>DEV, R_green=>R_green, R_atum=>R_atum, R_bacon=>R_bacon);  
+
+   sandwich_ft: entity work.sanduiche_tmr2 port map (clock=>clock, reset=>reset,
+              M100=>M100, DEV=>DEV, R_green=>R_green, R_atum=>R_atum, R_bacon=>R_bacon);  
+             
+    p1: process     --- read the  ROM  with the stimulus
+    begin
+   
+       wait for 10 ns;    -- delay to start the operations
+       wait until rising_edge(clock);
+      
+       for i in 0 to program'high loop   
+                M100      <= program(i)(4);
+                DEV       <= program(i)(3);
+                R_green   <= program(i)(2);
+                R_atum    <= program(i)(1);
+                R_bacon   <= program(i)(0);
+                wait until rising_edge(clock);
+                --wait for 5 ns;
+                --if program(i) /= "00000" then  -- trata entrada nula
+                --     wait until busy='0';
+                --end if;
+                --wait for 5 ns;
+       end loop;
+
+       wait for 20 ns;    -- delay to finish the operations
+
+       assert false
+            report "A SIMULACAO TERMINOU!"
+            severity failure;
+                 
+     end process;
+  
+end tb;
